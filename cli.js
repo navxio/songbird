3af5b11c56693ed9e1fa90571f6e9af5ba4cdf1f
Nav Saini <navdeep@mailbox.org>
2021-05-26 23:02:41 +0530

    implement get/set with optimist

--- cli.js
+++ cli.js
@@ -4,8 +4,6 @@ const { get, set, all } = require('.')
 const clipboard = require('clipboardy')
 const args = require('optimist').argv
 
-console.log('args', args)
-
 switch(args._.length) {
   case 0:
     all().then(list => {
@@ -17,10 +15,35 @@ switch(args._.length) {
     // get
     get(args._[0]).then(val => {
       if (args.p) {
+        console.log(val);
+      } else {
+        if (val) {
+          process.stdout.write(val)
+        }
+      }
+      if (!args.n) {
+        if (val) {
+          clipboard.writeSync(val)
+        }
       }
     })
     break;
   case 2:
+    // get
+    set(args._[0], args._[1]).then(val => {
+      if (args.p) {
+        console.log(val);
+      } else {
+        if (val) {
+          process.stdout.write(val)
+        }
+      }
+      if (!args.n) {
+        if (val) {
+          clipboard.writeSync(val)
+        }
+      }
+    })
     break;
   default:
     console.error('bad number of arguments')
