cb862714f8dcb262057afb5694198cd06809c6e1
Nav Saini <navdeep@mailbox.org>
2021-05-27 21:53:08 +0530

    shuffle

--- configureStore.js
+++ configureStore.js
@@ -5,7 +5,7 @@ const storeDir = path.resolve(process.env.HOME, '.songbird')
 
 const dbPath = path.resolve(process.env.HOME, '.songbird', 'db.sqlite3')
 
-const createStorageDir = async () => {
+const configureStore = async () => {
   try {
     await fs.ensureDir(storeDir)
     const db = require('better-sqlite3')(dbPath)
@@ -18,4 +18,4 @@ const createStorageDir = async () => {
   } 
 }
 
-createStorageDir()
+configureStore()
